6d0ae060161fc227a8b2c1cef0405b6fd0688855
Marcin Zareba <marcin.zareba1@gmail.com>
2016-03-19 00:05:16 +0100

    Angular added

---
+++ ch-webapp/src/main/resources/static/assets/app/app.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Marcin on 18.03.2016.
+ */
+
+var app = angular.module('challengeAccepted', ['ui.router']);
+
+app.config(function ($urlRouterProvider, $stateProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.state('home', {
+        url: '/',
+        templateUrl: 'assets/app/home/home.html'
+    })
+        .state('about', {
+            url: '/about',
+            templateUrl: 'assets/app/about/about.html',
+            controller: function () {
+
+            }
+        });
+
+});
