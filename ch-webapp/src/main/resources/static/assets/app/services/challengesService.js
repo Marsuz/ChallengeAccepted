74f0e82088f4c1a37c4c41e21ae5666c998a5580
ArasAwas <seyuioa@gmail.com>
2016-03-19 04:58:09 +0100

    added my-challenges controller

---
+++ ch-webapp/src/main/resources/static/assets/app/services/challengesService.js
@@ -0,0 +1,176 @@
+/**
+ * Created by Sara on 2016-03-19.
+ */
+
+app.factory('Challenges', function(){
+    var database = {newchallenges: [
+        {
+            id: 1,
+            name: "Wez Grabcia na silke",
+            owner: 'Zajda',
+            expirationdate: '23.03.2016',
+            image: 'small_cup.png',
+            participants: [
+                {
+                    id: 1,
+                    name: 'Shimmy'
+                }
+            ],
+            invited: [
+                {
+                    id: 1,
+                    name: 'Zajda'
+                }
+            ]
+        },
+        {
+            id: 2,
+            name:"Wypij jak najwiecej energoli",
+            owner: 'Shimmy',
+            expirationdate: '23.03.2016',
+            image: 'small_podium.jpg',
+            participants: [
+                {
+                    id: 1,
+                    name: 'Marcin'
+                }
+            ],
+            invited: [
+                {
+                    id: 1,
+                    name: 'Zajda'
+                }
+            ]
+        }
+    ],
+    acceptedchallenges: [
+        {
+            id: 1,
+            name: "BrainCode",
+            owner: 'Zajda',
+            expirationdate: '23.03.2016',
+            image: 'small_podium.jpg',
+            participants: [
+                {
+                    id: 1,
+                    name: 'Shimmy'
+                }
+            ],
+            invited: [
+                {
+                    id: 1,
+                    name: 'Zajda'
+                }
+            ]
+        },
+        {
+            id: 2,
+            name:"ULTRICIES SED MAGNA EUISMOD ENIM VITAE GRAVIDA",
+            owner: 'Shimmy',
+            expirationdate: '23.03.2016',
+            image: 'small_cup.png',
+            participants: [
+                {
+                    id: 1,
+                    name: 'Marcin'
+                }
+            ],
+            invited: [
+                {
+                    id: 1,
+                    name: 'Zajda'
+                }
+            ]
+        }
+    ],
+        rejectedchallenges: [
+            {
+                id: 1,
+                name: "3 warunki w tym semestrze",
+                owner: 'Zajda',
+                expirationdate: '23.03.2016',
+                image: 'small_podium.jpg',
+                participants: [
+                    {
+                        id: 1,
+                        name: 'Shimmy'
+                    }
+                ],
+                invited: [
+                    {
+                        id: 1,
+                        name: 'Zajda'
+                    }
+                ]
+            },
+            {
+                id: 2,
+                name:"NIE WYŁĄCZAJ KOMPA PRZEZ X DNI",
+                owner: 'Shimmy',
+                expirationdate: '23.03.2016',
+                image: 'small_cup.png',
+                participants: [
+                    {
+                        id: 1,
+                        name: 'Marcin'
+                    }
+                ],
+                invited: [
+                    {
+                        id: 1,
+                        name: 'Zajda'
+                    }
+                ]
+            }
+        ],
+        finishedchallenges: [
+            {
+                id: 1,
+                name: "Aghacks",
+                owner: 'Zajda',
+                expirationdate: '23.03.2016',
+                image: 'small_podium.jpg',
+                participants: [
+                    {
+                        id: 1,
+                        name: 'Shimmy'
+                    }
+                ],
+                invited: [
+                    {
+                        id: 1,
+                        name: 'Zajda'
+                    }
+                ]
+            },
+            {
+                id: 2,
+                name:"costam",
+                owner: 'Shimmy',
+                expirationdate: '23.03.2016',
+                image: 'small_cup.png',
+                participants: [
+                    {
+                        id: 1,
+                        name: 'Marcin'
+                    }
+                ],
+                invited: [
+                    {
+                        id: 1,
+                        name: 'Zajda'
+                    }
+                ]
+            }
+        ]
+        
+    };
+
+
+    return {
+        db: database,
+        add: function (challenge) {
+            database.newchallenges.push(challenge);
+        }
+    };
+})
