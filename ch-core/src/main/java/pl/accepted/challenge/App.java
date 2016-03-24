c4b0badeafe8e49dc893461300335f3d2275510b
marcin <majewskimarcin@outlook.com>
2016-03-24 22:50:58 +0100

    -Working Spring Data version

--- ch-core/src/main/java/pl/accepted/challenge/App.java
+++ ch-core/src/main/java/pl/accepted/challenge/App.java
@@ -1,9 +1,12 @@
 package pl.accepted.challenge;
 
+import challenges.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = User.class)
 public class App {
 
 	public static void main(String[] args) {
