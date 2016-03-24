8bb885afecd10e1d618183e5257f97c176e174b9
marcin <majewskimarcin@outlook.com>
2016-03-25 00:30:20 +0100

    -Optimizing @EntityScan

--- ch-core/src/main/java/pl/accepted/challenge/App.java
+++ ch-core/src/main/java/pl/accepted/challenge/App.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackageClasses = { User.class, FirstWinChallenge.class })
+@EntityScan(basePackages = "challenges")
 public class App {
 
 	public static void main(String[] args) {
