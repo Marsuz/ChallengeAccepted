4f1106b00bcce9beff82c0a2d19aef9b8ff7fe42
Wiktor <wiktor.jurasz@gmail.com>
2016-03-25 00:24:34 +0100

    Crud user repository fix

--- ch-core/src/main/java/pl/accepted/challenge/persistence/UserRepository.java
+++ ch-core/src/main/java/pl/accepted/challenge/persistence/UserRepository.java
@@ -3,7 +3,7 @@ package pl.accepted.challenge.persistence;
 import challenges.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, String> {
 
     User findByUsername(String username);
 }
