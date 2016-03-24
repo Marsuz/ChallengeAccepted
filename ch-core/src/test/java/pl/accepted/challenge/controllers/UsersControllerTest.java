dae2fa400204ba7cd206a04d2389e0cab0d6130c
marcin <majewskimarcin@outlook.com>
2016-03-25 00:59:15 +0100

    -Adding mockito and rest reassured

---
+++ ch-core/src/test/java/pl/accepted/challenge/controllers/UsersControllerTest.java
@@ -0,0 +1,34 @@
+package pl.accepted.challenge.controllers;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UsersControllerTest {
+
+	@BeforeClass
+	public static void setUp() {
+
+	}
+
+	@Test
+	public void getAllUsers() throws Exception {
+
+	}
+
+	@Test
+	public void getOneUser() throws Exception {
+
+	}
+
+	@Test
+	public void createUser() throws Exception {
+
+	}
+
+	@Test
+	public void deleteUser() throws Exception {
+
+	}
+}
