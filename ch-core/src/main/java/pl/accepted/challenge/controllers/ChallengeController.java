003332fed27ee844b1753e205cde418ab679e2e1
Marsuz <marcin.zajda@gmail.com>
2016-03-25 01:19:24 +0100

    Added FirstWinService and updated ChallengeController

--- ch-core/src/main/java/pl/accepted/challenge/controllers/ChallengeController.java
+++ ch-core/src/main/java/pl/accepted/challenge/controllers/ChallengeController.java
@@ -7,8 +7,8 @@ import pl.accepted.challenge.exceptions.ChallengeNotFoundException;
 import pl.accepted.challenge.exceptions.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import pl.accepted.challenge.persistence.FirstWinRepository;
-import pl.accepted.challenge.persistence.UserRepository;
+import pl.accepted.challenge.service.FirstWinService;
+import pl.accepted.challenge.service.UsersService;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -19,22 +19,22 @@ import java.util.List;
 public class ChallengeController {
 
     @Autowired
-    FirstWinRepository firstWinRepository;
+    FirstWinService firstWinService;
 
     @Autowired
-    UserRepository userRepository;
+    UsersService usersService;
 
     @RequestMapping(value = "/all", method = RequestMethod.GET)
     public List<FirstWinChallenge> getAllChallenges() {
 
-        return (List) firstWinRepository.findAll();
+        return firstWinService.getAllFirstChallenges();
 
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public FirstWinChallenge addChallenge(@RequestParam("name") String name, @RequestParam("deadline") String deadline, @RequestBody User user) throws ChallengeAlreadyExistsException{
 
-        FirstWinChallenge challenge = firstWinRepository.findOne(name);
+        FirstWinChallenge challenge = firstWinService.getFirstWinChallengeByName(name);
 
         if(challenge != null)  throw new ChallengeAlreadyExistsException();
 
@@ -42,7 +42,7 @@ public class ChallengeController {
         LocalDateTime deadlineAsLocalDateTime = LocalDateTime.parse(deadline, formatter);
 
         challenge = new FirstWinChallenge(name, user, deadlineAsLocalDateTime);
-        firstWinRepository.save(challenge);
+        firstWinService.createOrUpdate(challenge);
 
         return challenge;
 
@@ -51,40 +51,40 @@ public class ChallengeController {
     @RequestMapping(value = "/{name}")
     public FirstWinChallenge getChallenge(@RequestParam("name") String name) {
 
-        return firstWinRepository.findOne(name);
+        return firstWinService.getFirstWinChallengeByName(name);
 
     }
 
     @RequestMapping(value = "/{name}/add", method = RequestMethod.POST)
     public void addParticipantToChallenge(@RequestParam("name") String name, @RequestParam("username") String participant) throws UserNotFoundException, ChallengeNotFoundException {
 
-        FirstWinChallenge challenge = firstWinRepository.findOne(name);
+        FirstWinChallenge challenge = firstWinService.getFirstWinChallengeByName(name);
 
         if(challenge == null) throw new ChallengeNotFoundException();
 
-        User user = userRepository.findOne(participant);
+        User user = usersService.getUserByName(participant);
 
         if(user == null) throw new UserNotFoundException();
 
         challenge.addParticipant(user);
-        firstWinRepository.save(challenge);
+        firstWinService.createOrUpdate(challenge);
 
     }
 
     @RequestMapping(value = "/{name}/finish}", method = RequestMethod.POST)
     public void finishChallenge(@RequestParam("name") String name, @RequestParam("username") String winner) throws UserNotFoundException, ChallengeNotFoundException {
 
-        FirstWinChallenge challenge = firstWinRepository.findOne(name);
+        FirstWinChallenge challenge = firstWinService.getFirstWinChallengeByName(name);
 
         if(challenge == null) throw new ChallengeNotFoundException();
 
-        User user = userRepository.findOne(winner );
+        User user = usersService.getUserByName(winner);
 
         if(user == null) throw new UserNotFoundException();
 
         challenge.finishChallenge(user);
 
-        firstWinRepository.save(challenge);
+        firstWinService.createOrUpdate(challenge);
 
     }
 
---
+++ ch-core/src/main/java/pl/accepted/challenge/service/FirstWinService.java
@@ -0,0 +1,35 @@
+package pl.accepted.challenge.service;
+
+import challenges.FirstWinChallenge;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.accepted.challenge.persistence.FirstWinRepository;
+
+import java.util.List;
+
+/**
+ * Created by Marcin on 2016-03-25.
+ */
+
+@Service
+public class FirstWinService {
+
+    @Autowired
+    FirstWinRepository firstWinRepository;
+
+    public List<FirstWinChallenge> getAllFirstChallenges() {
+        return (List) firstWinRepository.findAll();
+    }
+
+    public void createOrUpdate(FirstWinChallenge challenge) {
+        firstWinRepository.save(challenge);
+    }
+
+    public FirstWinChallenge getFirstWinChallengeByName(String name) {
+        return firstWinRepository.findOne(name);
+    }
+
+    public void deleteFirstWinChallenge(FirstWinChallenge challenge) {
+        firstWinRepository.delete(challenge);
+    }
+}
