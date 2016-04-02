02f0a9eba9549e7c3e856a0ce94ba4c2470f85a1
Marsuz <marcin.zajda@gmail.com>
2016-04-02 13:18:35 +0200

    minor fixes

--- ch-core/src/main/java/pl/accepted/challenge/controllers/LoggingController.java
+++ ch-core/src/main/java/pl/accepted/challenge/controllers/LoggingController.java
@@ -7,19 +7,20 @@ import pl.accepted.challenge.exceptions.UserAlreadyExistsException;
 import pl.accepted.challenge.services.LoggingService;
 
 @RestController
+@RequestMapping(value = "/users")
 public class LoggingController {
 
     @Autowired
     private LoggingService loggingService;
 
-    @RequestMapping(value = "/users/login", method = RequestMethod.POST)
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
     @ResponseBody
     public User getLoggedUser(@RequestParam("username") String username, @RequestParam("password") String password) {
         return loggingService.findUser(username, password);
     }
 
 
-    @RequestMapping(value = "/users/register", method = RequestMethod.POST)
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
     @ResponseBody
     public User registerUser(@RequestBody User user) throws UserAlreadyExistsException {
 
